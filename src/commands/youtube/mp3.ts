6497b0bdcfdd2468300e19c5a5cb4d9157d5cc10
dan <d@zurly.me>
2024-07-15 14:43:41 +0100

    added youtube mp3/4 conversions

---
+++ src/commands/youtube/mp3.ts
@@ -0,0 +1,52 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { CommandConfig } from "../../classes/CommandConfig";
+import { Command } from "../../classes/Command";
+import { Readable } from "stream";
+
+const commandBuilder = new SlashCommandBuilder().setName("mp3").setDescription("Download a YouTube video as an MP3 file.");
+commandBuilder.addStringOption((option) => option.setName("link").setDescription("The YouTube video link").setRequired(true));
+
+const commandConfig = new CommandConfig(commandBuilder);
+
+const command = async (interaction: ChatInputCommandInteraction) => {
+    const link = interaction.options.getString("link");
+
+    if (!link) {
+        return await interaction.reply({ content: "Please provide a valid YouTube link.", ephemeral: true });
+    }
+
+    try {
+        const response = await fetch("https://media-download-api.onrender.com/youtube/downloadMp3", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ link }),
+        });
+
+        if (!response.ok) {
+            const errorText = await response.text();
+            throw new Error(`HTTP error! Status: ${response.status}, Message: ${errorText}`);
+        }
+
+        const arrayBuffer = await response.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+
+        const readableStream = new Readable();
+        readableStream._read = () => {};
+        readableStream.push(buffer);
+        readableStream.push(null);
+
+        const attachment = {
+            attachment: readableStream,
+            name: "audio.mp3",
+        };
+
+        await interaction.reply({ files: [attachment] });
+    } catch (error: any) {
+        console.error("Error fetching MP3 file:", error);
+        await interaction.reply({ content: `There was an error fetching the MP3 file: ${error.message}`, ephemeral: true });
+    }
+};
+
+export default new Command(commandConfig, command);
